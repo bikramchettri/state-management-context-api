205ec927926950cffcc99edc4e7544b5e6ae2918
bikramchettri <bikramchettri120@outlook.com>
2021-06-05 07:22:45 +0530

    1 file added

---
+++ src/AddMovie.js
@@ -0,0 +1,11 @@
+import React from 'react'
+export const AddMovie =() =>{
+  return(
+    <>
+    <form>
+    <input type="text" onChange={} value={movie}/>
+    <input type="text" onChange={} value={price} />
+    </form>
+    </>
+  )
+}
